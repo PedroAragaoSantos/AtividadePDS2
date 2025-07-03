8eb303a3208e9c2580e14d896b03775937ed2b52
Pedro <ph1953785@gmail.com>
2025-07-03 11:33:06 -0300

    PDS ATT

---
+++ PDS2-I6-25-1/LojaMVC/src/main/java/controller/ClienteViewController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class ClienteViewController {
+
+    @FXML
+    private Button btnSalvar;
+
+    @FXML
+    private TextField txtDataNascimento;
+
+    @FXML
+    private TextField txtEndereco;
+
+    @FXML
+    private TextField txtNome;
+
+    @FXML
+    private TextField txtTelefone;
+
+    @FXML
+    void btnSalvarOnActionPerformed(ActionEvent event) {
+
+    }
+
+}
